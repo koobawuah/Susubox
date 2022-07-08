ff98132577ecd5d14c92b7d58b2c7e61b58f701e
koobawuah <me@bawuahboakye.com>
2022-07-08 22:47:42 +0000

    Updating react-native version of susubox app

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator} from '@react-navigation/stack';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import StackNavigation from './navigation/StackNavigation';
 
+const HomeStack = createStackNavigator();
 
-const HomeStack = createStackNavigator(); 
-
-
-const App = () =>  {
-
+const App = () => {
   return (
-    <NavigationContainer theme={{dark: false, colors: { background: 'rgba(255, 255, 255, 1)',}}}>
+    <NavigationContainer
+      theme={{dark: false, colors: {background: 'rgba(255, 255, 255, 1)'}}}>
       <StatusBar barStyle="dark-content" />
-      <HomeStack.Navigator  headerMode='none'>
-        <HomeStack.Screen name='Modal' component={StackNavigation} />
+      <HomeStack.Navigator headerMode="none">
+        <HomeStack.Screen name="Modal" component={StackNavigation} />
       </HomeStack.Navigator>
     </NavigationContainer>
-  )
-} 
-
+  );
+};
 
 export default App;
--- constants/Data.js
+++ constants/Data.js
@@ -1,44 +1,52 @@
-const Data = [ 
-    {
-      id: 1,
-      title: '10 Tips',
-      body: [
-        "1. Be very stingy in this life, or the next, or the next... ",
-        "2. Don\’t be afraid or be shy to say \‘No, I don\’t have money.",
-        "3. Always live below your means.", 
-        "4. Think about the end goal, you will be rich soon.", 
-        "5. Before you spend... just don\’t spend!",
-        "6. Flee from all temptations.",
-        "7. Don’t treat yourself to nice things, it’s a trap!",
-        "8. Because you have it today, doesn\’t mean you have it tomorrow.",
-        "9. Always keep track of your spending",
-        "10. Beware of speding money on periodic purchases like gifts, vacation, you know the rest"
-      ],
-    },
-    {
-      id: 2,
-      title: 'Increase Income',
-      body: [
-        "1. Become a farmer. ",
-        "2. Work a 9 - 5 job to earn stable money('salary')",
-        "3. Take calculated risks for new business ideas, at least every 6 months", 
-        "4. Believe you can have more than one income source", 
-        "5. Request for a raise at your current job",
-      ],
-    },
-    {
-      id: 3,
-      title: 'Coming Features...',
-      body: [
-        "We can help you save! By following our savings a day calculations, you can achieve your money goals ",
-        "1. Save daily on this app through your mobile money and bank card deposits",
-        "2. Deposit cash of your daily targets at any of our official vendors", 
-        "3. We will save for you by depositing in cash if you miss a day, but we take 2% back when you are ready to withraw, when your target is reached!", 
-        " \n \nThis will be the best savings app you will ever encounter",
-      ],
-      
-    },
-  ]
+// Hi there! I’m a bike messenger by day, aspiring millionaire by night, all thanks to the magnificent savings skills I got from using Susubox App. I live in Nima, Accra Ghana but will be moving to Trasaco soon. (Don’t sleep on this app.)
 
-  
-  export default Data;
+// Nii Papafio Lamptey
+
+// …still not convinced?
+
+// Since Susubox, I have been able to make bold financial moves because I have already seen the future!. Yes, that’s what it is… this app allows you to see into the future of your finances. Things have never been the same since!
+
+// Kwame Ntiamoah
+
+const Data = [
+  {
+    id: 1,
+    title: '10 Tips',
+    body: [
+      '1. Be very stingy in this life, or the next, or the next... ',
+      '2. Don’t be afraid or be shy to say ‘No, I don’t have money.',
+      '3. Always live below your means.',
+      '4. Think about the end goal, you will be rich soon.',
+      '5. Before you spend... just don’t spend!',
+      '6. Flee from all temptations.',
+      '7. Don’t treat yourself to nice things, it’s a trap!',
+      '8. Because you have it today, doesn’t mean you have it tomorrow.',
+      '9. Always keep track of your spending',
+      '10. Beware of spending money on periodic purchases like gifts, vacation, you know the rest',
+    ],
+  },
+  {
+    id: 2,
+    title: 'Increase Income',
+    body: [
+      '1. Become a farmer. ',
+      "2. Work a 9 - 5 job to earn stable money('salary')",
+      '3. Take calculated risks for new business ideas, at least every 6 months',
+      '4. Believe you can have more than one income source',
+      '5. Request for a raise at your current job',
+    ],
+  },
+  {
+    id: 3,
+    title: 'Coming Features...',
+    body: [
+      'We can help you save! By following our savings a day calculations, you can achieve your money goals ',
+      '1. Save daily on this app through your mobile money and bank card deposits',
+      '2. Deposit cash of your daily targets at any of our official vendors',
+      '3. We will save for you by depositing in cash if you miss a day, but we take 2% back when you are ready to withraw, when your target is reached!',
+      ' \n \nThis will be the best savings app you will ever encounter',
+    ],
+  },
+];
+
+export default Data;
